102a5386f94b844250c5365f5d9d4334cd535a43
Shaykhullin <yolorusher@gmail.com>
2018-02-28 14:36:46 +0300

    2 Лаба

--- Labs/Lab2/Lab2/MainWindow.Designer.cs
+++ Labs/Lab2/Lab2/MainWindow.Designer.cs
@@ -28,12 +28,51 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.axShockwaveFlash1);
+			this.groupBox1.Location = new System.Drawing.Point(43, 32);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(1221, 682);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// axShockwaveFlash1
+			// 
+			this.axShockwaveFlash1.Enabled = true;
+			this.axShockwaveFlash1.Location = new System.Drawing.Point(413, 363);
+			this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+			this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+			this.axShockwaveFlash1.Size = new System.Drawing.Size(412, 338);
+			this.axShockwaveFlash1.TabIndex = 0;
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1315, 745);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "MainWindow";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.MainWindow_Load);
+			this.groupBox1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
 	}
 }
 
--- Labs/Lab2/Lab2/MainWindow.cs
+++ Labs/Lab2/Lab2/MainWindow.cs
@@ -1,80 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static Lab2.OLE;
-using static Lab2.OLE.CLSCTX;
 
 namespace Lab2
 {
-	public partial class MainWindow : Form
+	public partial class MainWindow : Form, IOleClientSite, IOleInPlaceSite
 	{
+		private tagRECT NewRect = new tagRECT(100, 100, 100, 100);
+		private tagRECT New = new tagRECT(0, 0, 1250, 920);
+
 		public MainWindow()
 		{
-			CoInitializeEx(IntPtr.Zero, 2);
+			OLE.CoInitializeEx(IntPtr.Zero, 2);
 			InitializeComponent();
 
-			var browser = CoCreateInstance(CLASS_WEBBROWSER, null, CLSCTX_INPROC_SERVER, IID_IWebBrowser2) as IOleObject;
-
-			var site = new ClientSite(Handle);
-
-			browser.SetClientSite(site);
-
-			tagRECT rectangle = new tagRECT
-			{
-				Top = 10,
-				Bottom = 10,
-				Right = 10,
-				Left = 10
-			};
-
-			browser.DoVerb((int)OLEDOVERB.OLEIVERB_INPLACEACTIVATE, IntPtr.Zero, site, 0, Handle, ref rectangle);
-		}
-	}
-
-	public class ClientSite : IOleClientSite, IOleInPlaceSite
-	{
-		private IntPtr handle;
-
-		public ClientSite(IntPtr handle)
-		{
-			this.handle = handle;
-		}
-
-		//IOLEInPlaceSite
-		[return: MarshalAs(UnmanagedType.I4)]
-		public int OnPosRectChange([In, MarshalAs(UnmanagedType.Struct)] ref tagRECT lprcPosRect)
-		{
-			lprcPosRect = new tagRECT
-			{
-				Top = 10,
-				Bottom = 10,
-				Right = 10,
-				Left = 10
-			};
-
-			return HRESULT.S_OK;
-		}
-
-		[return: MarshalAs(UnmanagedType.I4)]
-		public int GetWindow([In, Out] ref IntPtr phwnd)
-		{
-			phwnd = handle;
-
-			return HRESULT.S_OK;
-		}
-
-		[return: MarshalAs(UnmanagedType.I4)]
-		public int GetWindowContext([MarshalAs(UnmanagedType.Interface), Out] out IOleInPlaceFrame ppFrame, [MarshalAs(UnmanagedType.Interface), Out] out IOleInPlaceUIWindow ppDoc, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagRECT lprcPosRect, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagRECT lprcClipRect, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagOIFI lpFrameInfo)
-		{
-			throw new NotImplementedException();
+			axShockwaveFlash1.Movie = Application.StartupPath + @"\tuma.swf";
 		}
 
 		[return: MarshalAs(UnmanagedType.I4)]
@@ -87,14 +28,14 @@ namespace Lab2
 		public int GetMoniker([In, MarshalAs(UnmanagedType.U4)] uint dwAssign, [In, MarshalAs(UnmanagedType.U4)] uint dwWhichMoniker, [MarshalAs(UnmanagedType.Interface), Out] out IMoniker ppmk)
 		{
 			ppmk = null;
-			return HRESULT.ERROR_ACCESS_DENIED;
+			return HRESULT.E_FAIL;
 		}
 
 		[return: MarshalAs(UnmanagedType.I4)]
 		public int GetContainer([MarshalAs(UnmanagedType.Interface), Out] out IOleContainer ppContainer)
 		{
 			ppContainer = null;
-			return HRESULT.ERROR_ACCESS_DENIED;
+			return HRESULT.E_NOINTERFACE;
 		}
 
 		[return: MarshalAs(UnmanagedType.I4)]
@@ -112,6 +53,13 @@ namespace Lab2
 		[return: MarshalAs(UnmanagedType.I4)]
 		public int RequestNewObjectLayout()
 		{
+			return HRESULT.S_OK;
+		}
+
+		[return: MarshalAs(UnmanagedType.I4)]
+		public int GetWindow([In, Out] ref IntPtr phwnd)
+		{
+			phwnd = groupBox1.Handle;
 			return HRESULT.S_OK;
 		}
 
@@ -138,9 +86,18 @@ namespace Lab2
 		{
 			return HRESULT.S_OK;
 		}
-
 		[return: MarshalAs(UnmanagedType.I4)]
-		public int Scroll([In, MarshalAs(UnmanagedType.Struct)] ref tagSIZE scrollExtent)
+		public int GetWindowContext([MarshalAs(UnmanagedType.Interface), Out] out IOleInPlaceFrame ppFrame, [MarshalAs(UnmanagedType.Interface), Out] out IOleInPlaceUIWindow ppDoc, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagRECT lprcPosRect, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagRECT lprcClipRect, [In, MarshalAs(UnmanagedType.Struct), Out] ref tagOIFI lpFrameInfo)
+		{
+			ppFrame = null;
+			ppDoc = null;
+			lprcPosRect = New;
+			lprcClipRect = New;
+
+			return HRESULT.S_OK;
+		}
+
+		int IOleInPlaceSite.Scroll(ref tagSIZE scrollExtent)
 		{
 			return HRESULT.S_OK;
 		}
@@ -167,6 +124,28 @@ namespace Lab2
 		public int DeactivateAndUndo()
 		{
 			return HRESULT.S_OK;
+		}
+
+		[return: MarshalAs(UnmanagedType.I4)]
+		public int OnPosRectChange([In, MarshalAs(UnmanagedType.Struct)] ref tagRECT lprcPosRect)
+		{
+			lprcPosRect = New;
+			return HRESULT.S_OK;
+		}
+
+		private void MainWindow_Load(object sender, EventArgs e)
+		{
+			var instance = OLE.CoCreateInstance(OLE.CLASS_WEBBROWSER, null, OLE.CLSCTX.CLSCTX_INPROC_SERVER, OLE.IID_IWebBrowser2);
+
+			var obj = instance as IOleObject;
+
+			obj.SetClientSite(this);
+
+			var rect = new tagRECT();
+			obj.DoVerb((int)OLEDOVERB.OLEIVERB_INPLACEACTIVATE, IntPtr.Zero, this, 0, Handle, ref rect);
+
+			var browser = instance as IWebBrowser2;
+			browser.Navigate("http://google.com", null, null, null, null);
 		}
 	}
 }
