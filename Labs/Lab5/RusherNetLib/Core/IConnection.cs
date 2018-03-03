a8e88a23f4aa6359e6033ba3b4c83e277036cf04
Sergey <yolorusher@gmail.com>
2018-03-03 18:50:43 +0300

    Lab5

---
+++ Labs/Lab5/RusherNetLib/Core/IConnection.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+namespace RusherNetLib.Core
+{
+	public interface IConnection : IPeer
+	{
+		SocketError SocketError { get; }
+		IMessage CreateMessage();
+		void Send(IMessage message);
+		void Disconnect();
+	}
+}
---
+++ Labs/Lab5/RusherNetLib/Message.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using RusherNetLib.Core;
+
+namespace RusherNetLib
+{
+	public class Message : IMessage
+	{
+		public int Sended { get; }
+		public int Received { get; }
+
+		private readonly Dictionary<string, object> messages;
+		private object outValue;
+
+		internal Message()
+		{
+			Sended = 0;
+			Received = 0;
+			messages = new Dictionary<string, object>();
+		}
+
+		internal Message(int sended) : this()
+		{
+			Sended = sended;
+		}
+
+		internal Message(byte[] data) : this()
+		{
+			Received = data.Length;
+			var xml = XElement.Parse(Encoding.UTF8.GetString(data)).Descendants("f");
+			messages = xml.ToDictionary(x => x.Attribute("n").Value, x => (object)x.Attribute("v").Value);
+		}
+
+		public byte[] GetBytes()
+		{
+			var xml = new XElement("msg",
+				messages.Select(x => new XElement("f", new XAttribute("n", x.Key), new XAttribute("v", x.Value))));
+			return Encoding.UTF8.GetBytes(xml.ToString());
+		}
+
+		public object this[string name]
+		{
+			get
+			{
+				if (messages.TryGetValue(name, out outValue))
+					return outValue;
+
+				throw new Exception("Обьект не найден");
+			}
+			set => messages.Add(name, value);
+		}
+	}
+}
